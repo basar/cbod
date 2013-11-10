faf45a4eef667a46cf3fd66b57031c5fc2bcd34f
basar <basarcetinkaya@gmail.com>
2013-11-10 21:03:26 +0200

    bug fixes added new part types

--- net.bsrc.cbod/src/main/java/net/bsrc/cbod/main/Main.java
+++ net.bsrc.cbod/src/main/java/net/bsrc/cbod/main/Main.java
@@ -43,7 +43,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-	    //saveImageModelsToDB();
+	    saveImageModelsToDB();
 
 		// Edge histogram descriptors (best result for tire)
 		testSVM(EDescriptorType.EHD, CBODConstants.CAR_WINDOW_PART);
